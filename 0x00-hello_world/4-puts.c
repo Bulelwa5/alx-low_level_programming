62371eb1b725e8b0d2ece5493405e2c18791748b
Bulelwa5 <nombulelobule5@gmail.com>
2023-06-15 19:19:02 -0700

    new cfile

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -entry point 
+ *
+ * description: A C program that with put function 
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0)
