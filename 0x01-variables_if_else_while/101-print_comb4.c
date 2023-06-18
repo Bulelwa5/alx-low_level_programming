870b6d8760645e0336d61866920539da7f9b21d3
Bulelwa5 <nombulelobule5@gmail.com>
2023-06-18 13:20:02 -0700

    file edited

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prits all possible different combinations of three digits 
+ * main - prits all possible different combinations of three digits
  * Return: Always 0 (Success)
  */
 int main(void)
