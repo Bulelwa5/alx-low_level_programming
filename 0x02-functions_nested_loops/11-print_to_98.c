ebcdf7b52dd947e6719fc95ecfcab9ca2df18466
Bulelwa5 <nombulelobule5@gmail.com>
2023-06-20 12:04:47 -0700

    file fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,10 +15,10 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				printf("%d, ", n--);
+				printf("%d, ", i);
 			else if (i == 98)
-				printf("%d\n", n);
-	}
+				printf("%d\n", i);
+		}
 	}	else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
