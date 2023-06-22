c11560f85e7cfc12d82c59723a5d3fb018f626de
Bulelwa5 <nombulelobule5@gmail.com>
2023-06-22 12:20:44 -0700

    new files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main -tests function that prints if integer is posive or negative 
+ * return: 0
+ */
+
+int main(void)
+{
+	int 1;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return(0);
+}
